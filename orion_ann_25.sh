e6c7baef0aa02806b42af18832f7b36b5fbb6c20
acmlia <lia.meteorologia@gmail.com>
2019-04-21 21:57:54 -0300

    updating from rainfall to orion

--- orion_ann_25.sh
+++ orion_ann_25.sh
@@ -16,7 +16,7 @@ workdir="/home/david/git-repos/ann_training"
 application="$(which python)"
 
 # Run options for the application
-options="$workdir/main_ann_24.py"
+options="$workdir/main_ann_25.py"
 
 ###############################################################
 ### You should not have to change anything below this line ####
